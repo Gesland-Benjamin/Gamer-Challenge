fd7f904e1b7c1c4c4781fbd5e366ade7f9297544
Orhunox <orhun.cel@gmail.com>
2025-09-26 13:51:14 +0200

    Setup project + index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.set("views", "./app/views");
+app.set("view engine", "ejs");
+
+app.use(express.static("./public"));
+
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}...`);
+});
