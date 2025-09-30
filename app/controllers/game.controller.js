7a5ad81679b1957b1860228f44ebe5c019545d4a
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-09-30 09:41:29 +0200

    getAll created + getById created in gameController

--- app/controllers/game.controller.js
+++ app/controllers/game.controller.js
@@ -1,17 +1,39 @@
- import { Game } from "../models/index.js"
+// Importe le modèle Game depuis les modèles
+import { Game } from "../models/index.js"
  
- export const gameController = {
+// Définition du contrôleur de jeu
+export const gameController = {
 
     // Afficher tous les jeux 
     async getAll(req, res) {
-
+        // Récupère tous les jeux, triés par nom (ordre croissant)
         const listGames = await Game.findAll({
             order: [
-                ["position", "ASC"]
+                ["name", "ASC"]
             ]
         });
 
-        res.render("game", { listGames })
+        // Affiche la vue "game" avec la liste des jeux
+        res.render("games", { listGames })
+    },
+
+    // Afficher les détails d'un jeu 
+    async getById(req,res) {
+
+        // récupération de l'id dans la requête
+        const { id } = req.params;
+        
+        // On vérifie que l'id du jeu est bien dans la BDD
+        const game = await Game.findByPk(id)
+
+        // Si le jeu n'existe pas, retourne une erreur 404
+        if (!game) {
+            return res.status(404).render({ error : "id is invalid."});
+        }
+
+        // Si ok on renvoie le jeu 
+        res.status(200).render("game", { game });
+
     }
 
 }
--- app/routes/game.router.js
+++ app/routes/game.router.js
@@ -1,7 +1,16 @@
+// Importe Router depuis express pour créer un routeur
 import { Router } from "express";
+// Importe le contrôleur des jeux
 import { gameController } from "../controllers/index.js";
+// Importe le middleware pour vérifier l'id
+import { checkId } from "../middlewares/checkId.middleware.js";
 
- export const gameRouter = Router();
+// Crée une instance de routeur pour les routes de jeux
+export const gameRouter = Router();
 
- gameRouter.get('/games', gameController.getAll);
+// Route pour récupérer tous les jeux
+gameRouter.get('/games', gameController.getAll);
+
+// Route pour récupérer un jeu par son id, avec vérification de l'id
+gameRouter.get('/games/:id', checkId, gameController.getById);
 
